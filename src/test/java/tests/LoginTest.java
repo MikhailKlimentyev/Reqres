fd9351143e9838e13059a803f709606cec09e16c
Mikhail Klimentyev <mikhail.klimentyev@gmail.com>
2020-11-15 18:33:04 +0300

    Add login test

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,48 +3,45 @@ package tests;
 import io.restassured.response.ValidatableResponse;
 import models.ErrorResponse;
 import models.LoginPayload;
-import models.TokenWithIdResponse;
+import models.TokenResponse;
 import org.apache.http.HttpStatus;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
-public class RegisterTest extends BaseTest {
+public class LoginTest extends BaseTest {
 
-    private static String REGISTER_URI = "register";
+    private static String LOGIN_URI = "login";
 
     @Ignore
     @Test
-    public void tokenShouldBeReturned() {
+    public void tokenShouldBeReturnedAfterSuccessfulLogin() {
         ValidatableResponse response = getRequestSpecification()
                 .log().all()
-                .body(new LoginPayload("eve.holt@reqres.in", "pistol"))
+                .body(new LoginPayload("eve.holt@reqres.in", "cityslicka"))
                 .when()
-                .post(String.format(urlPattern, URL, REGISTER_URI))
+                .post(String.format(urlPattern, URL, LOGIN_URI))
                 .then()
                 .log().all();
 
         int actualStatusCode = response.extract().statusCode();
         commonAssertion.validateStatusCode(actualStatusCode, HttpStatus.SC_OK);
 
-        TokenWithIdResponse tokenWithIdResponse = response.extract().body().as(TokenWithIdResponse.class);
-        int id = tokenWithIdResponse.getId();
-        commonAssertion.validateFieldNotBlank(String.valueOf(id));
-
-        String token = tokenWithIdResponse.getToken();
+        TokenResponse tokenResponse = response.extract().body().as(TokenResponse.class);
+        String token = tokenResponse.getToken();
         commonAssertion.validateField(token, "QpwL5tke4Pnpja7X4");
     }
 
     @Test
     public void badRequestResponseShouldBeReturnedIfPasswordNotPassed() {
         LoginPayload loginPayload = LoginPayload.builder()
-                .email("sydney@fife")
+                .email("peter@klaven")
                 .build();
 
         ValidatableResponse response = getRequestSpecification()
                 .log().all()
                 .body(loginPayload)
                 .when()
-                .post(String.format(urlPattern, URL, REGISTER_URI))
+                .post(String.format(urlPattern, URL, LOGIN_URI))
                 .then()
                 .log().all();
 
